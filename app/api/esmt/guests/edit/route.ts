1854431a1f87a5f9f013107cd7f0d6ceb5cdd964
Constantine Khamis <costikhamis@gmail.com>
2024-11-14 13:12:53 -0800

    Added new endpoint that will edit existing guests

--- app/api/esmt/guests/edit/route.ts
+++ app/api/esmt/guests/edit/route.ts
@@ -1,19 +1,21 @@
 import { PrismaClient } from '@prisma/client';
 import {NextRequest, NextResponse} from "next/server";
-import {createGuestSchema} from "@/components/ValidationSchemas";
+import {editGuestSchema} from "@/components/ValidationSchemas";
 
 
 const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest){
     const body = await request.json();
-    const validation = createGuestSchema.safeParse(body);
+    const validation = editGuestSchema.safeParse(body);
 
+    console.log(body);
     if(!validation.success){
         return NextResponse.json(validation.error.format(), {status: 400});
     }
 
-    const newGuest = await prisma.guest.create({
+    const newGuest = await prisma.guest.update({
+        where: { id:body.id },
         data: {firstName: body.firstName, lastName: body.lastName, email: body.email, discordId: body.discordId, phoneNumber: body.phoneNumber},
     })
     return NextResponse.json(newGuest, {status: 201});
