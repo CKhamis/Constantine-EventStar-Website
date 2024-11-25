05514e60e9a4f1f78b5c3d5a3fa4bf3f03e77034
Constantine Khamis <costikhamis@gmail.com>
2024-11-24 17:02:37 -0800

    Begun working on public calendar screen

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,11 +1,60 @@
 import TopBar from "@/components/TopBar";
 import Footer from "@/components/Footer";
+import {CalendarCheck2, CalendarClock, CalendarHeart, User} from "lucide-react";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 
 export default function Calendar(){
     return (
         <>
-            <TopBar />
-            <Footer />
+            <TopBar/>
+            <div className="container mt-4">
+                <div className="flex justify-between gap-4 mb-4">
+                    <h1 className="text-3xl">Event Calendar</h1>
+                </div>
+                <div className="mb-4 hidden lg:grid grid-cols-4 gap-4">
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">Total Events</CardTitle>
+                            <CalendarCheck2 className="-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold"></div>
+                            <p className="text-xs text-muted-foreground">Currently in database</p>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">RSVP Count</CardTitle>
+                            <User className="-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold"></div>
+                            <p className="text-xs text-muted-foreground"> have responded with yes</p>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">Most Common Meet Time</CardTitle>
+                            <CalendarClock className="-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold"></div>
+                            <p className="text-xs text-muted-foreground"></p>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">Favorite Event Type</CardTitle>
+                            <CalendarHeart className="-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold"></div>
+                            <p className="text-xs text-muted-foreground"></p>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
+            <Footer/>
         </>
 
     );
