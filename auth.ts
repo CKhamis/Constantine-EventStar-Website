35993ec8367f55622db4e1ce46454f616a4cb4e8
Constantine Khamis <costikhamis@gmail.com>
2024-12-22 21:15:55 -0800

    It is now possible to combine multiple accounts into one user. Also allows enrollment process

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import {PrismaClient} from "@prisma/client";
 import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 
 const prisma = new PrismaClient();
 
@@ -10,9 +11,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         strategy: "database",
     },
     adapter: PrismaAdapter(prisma),
-    providers: [GitHub],
+    providers: [GitHub, Google],
     callbacks: {
         async signIn({ user, account, profile }) {
+            console.log("signing in...")
+
             if (!account) {
                 console.error("Account is null or undefined");
                 return false; // Reject sign-in
@@ -26,14 +29,23 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
                 : null;
 
             if (existingUser) {
+                console.log("existing user...." + account.provider);
+
+                let r = await prisma.account.findMany({
+                    where: {
+                        userId: existingUser.id,
+                        provider: account.provider
+                    }
+                });
+
+                console.log(r);
                 // Link the account to the existing user
-                console.log("we seen you before!--------------------")
                 await prisma.account.upsert({
                     where: {
                         provider_providerAccountId: {
                             provider: account.provider,
                             providerAccountId: account.providerAccountId,
-                        },
+                        }
                     },
                     update: {
                         userId: existingUser.id,
@@ -45,33 +57,35 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
                         providerAccountId: account.providerAccountId,
                         access_token: account.access_token,
                         refresh_token: account.refresh_token,
-                        expires_at: account.expires_at,
+                        // expires_at: account.expires_at,
                     },
                 });
             } else {
-                // Customize account creation behavior here
-                const newUser = await prisma.user.create({
-                    data: {
-                        email: user.email,
-                        name: user.name,
-                        image: user.image,
-                        // Add other default fields or process as necessary
-                    },
-                });
-
-                await prisma.account.create({
-                    data: {
-                        userId: newUser.id,
-                        type: account.type,
-                        provider: account.provider,
-                        providerAccountId: account.providerAccountId,
-                        access_token: account.access_token,
-                        refresh_token: account.refresh_token,
-                        expires_at: account.expires_at,
-                    },
-                });
+                console.log("not existing user....");
+                // Users are only able to be added through manual enrollment in ESMT
+                return false;
+                // const newUser = await prisma.user.create({
+                //     data: {
+                //         email: user.email,
+                //         name: user.name,
+                //         image: user.image,
+                //         // Add other default fields or process as necessary
+                //     },
+                // });
+                //
+                // await prisma.account.create({
+                //     data: {
+                //         userId: newUser.id,
+                //         type: account.type,
+                //         provider: account.provider,
+                //         providerAccountId: account.providerAccountId,
+                //         access_token: account.access_token,
+                //         refresh_token: account.refresh_token,
+                //         expires_at: account.expires_at,
+                //     },
+                // });
             }
-            return true; // Continue with the sign-in process
+            return true;
         },
     },
 })
